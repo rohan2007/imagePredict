b504134be321d8ec7580427ff24f972e90d0b0e8
Rohan Kumar <71839773+rohan2007@users.noreply.github.com>
2020-12-15 13:06:18 +0530

    Update image_predict.py

--- image_predict.py
+++ image_predict.py
@@ -1,17 +1,36 @@
-from imageai.Detection import ObjectDetection
+"""
+This parameter allows you to parse in a function you will want to execute after
+each frame of the video is detected. If this parameter is set to a function, after every video
+frame is detected, the function will be executed with the following values parsed into it:
+-- position number of the frame
+-- an array of dictinaries, with each dictinary corresponding to each object detected.
+    Each dictionary contains 'name', 'percentage_probability' and 'box_points'
+-- a dictionary with with keys being the name of each unique objects and value
+   are the number of instances of each of the objects present
+-- If return_detected_frame is set to True, the numpy array of the detected frame will be parsed
+    as the fourth value into the function
+"""
+
+from imageai.Detection import VideoObjectDetection
 import os
-#Getting the working directory
-execution_path = os.getcwd()
+import cv2
 
-#Instanciating ObjectDetection and loading the model 
-detector = ObjectDetection()
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath( os.path.join(execution_path , "yolo.h5"))
-detector.loadModel()
+execution_path=os.getcwd()
 
-#Detecting object on the image
-detections, objects_path = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "image3new.jpg"), minimum_percentage_probability=30,  extract_detected_objects=True)
 
-#Looping for the results and printing the values
-for eachObject, eachObjectPath in zip(detections, objects_path):
-    print(eachObject["name"] , " : " , eachObject["percentage_probability"], " : ", eachObject["box_points"] )
+def forFrame(frame_number, output_array, output_count):
+    del output_count["person"]
+    print("FOR FRAME " , frame_number)
+    print("Output for each object : ", output_array)
+    print("Output count for unique objects : ", output_count)
+    print("------------END OF A FRAME --------------")
+
+camera = cv2.VideoCapture(0)
+
+video_detector = VideoObjectDetection()
+video_detector.setModelTypeAsYOLOv3()
+video_detector.setModelPath(os.path.join(execution_path, "yolo.h5"))
+video_detector.loadModel()
+
+
+video_detector.detectObjectsFromVideo(camera_input=camera, frames_per_second=20, per_frame_function=forFrame, minimum_percentage_probability=70, output_file_path="analyzed_video.mp4")
